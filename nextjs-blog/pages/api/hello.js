cb8b5108aec3699bbded31e5b29daf95d1d0371f
Patrick <patricktsaikl@gmail.com>
2023-03-13 12:15:09 +0800

    update hello API route

--- nextjs-blog/pages/api/hello.js
+++ nextjs-blog/pages/api/hello.js
@@ -1,3 +1,10 @@
 export default function hanlder(req, res) {
-  res.status(200).json({ text: "hello" });
+  if (req.method === "POST") {
+    return res.status(200).json({ text: "result of POST request" });
+  }
+  if (req.method === "GET") {
+    return res.status(200).json({ text: "result of GET request" });
+  }
+  console.log(req.method);
+  return res.status(404).json({ text: "only GET and POST are allowed" });
 }
